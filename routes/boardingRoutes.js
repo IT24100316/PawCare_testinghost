717ff66127413565dcb7bc37a6e43fc23b360df9
Pasindu Lakshan <IT24100316@my.sliit.lk>
2026-05-02 04:40:37 +0530

    feat: Boarding overhaul - multi-day date range booking with capacity management

--- routes/boardingRoutes.js
+++ routes/boardingRoutes.js
@@ -1,16 +1,24 @@
 const express = require('express');
-const { getAvailableSlots, lockSlot, confirmBooking, cancelBooking, getAllBoardingBookings, updateBookingStatus } = require('../controllers/boardingController');
+const {
+  getBoardingAvailability,
+  createBoardingBooking,
+  cancelBooking,
+  getAllBoardingBookings,
+  updateBookingStatus,
+} = require('../controllers/boardingController');
 const { protect, authorizeRoles } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 router.use(protect);
 
+// Manager / Admin routes
 router.get('/', authorizeRoles('BoardingManager', 'Admin'), getAllBoardingBookings);
 router.put('/:id/status', authorizeRoles('BoardingManager', 'Admin'), updateBookingStatus);
-router.get('/available', getAvailableSlots);
-router.post('/lock', lockSlot);
-router.post('/confirm', confirmBooking);
+
+// User routes
+router.get('/available', getBoardingAvailability);
+router.post('/book', createBoardingBooking);
 router.delete('/:id', cancelBooking);
 
 module.exports = router;
